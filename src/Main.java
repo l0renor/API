557a08c63c8cc7eb52fde50da477e937947b61e3
hm-Llukas <leon-lukas11@web.de>
2017-11-13 16:08:35 +0100

    bonus

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.awt.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,9 +11,9 @@ import java.util.concurrent.TimeUnit;
 //Bsp args: Agnes-Pockels-Bogen+21+80992+München 8:15 bicycling Ingolstädter+Str.+38+80992+München 10:00 driving Bunzlauer+Str.+8+80992+München 8:30 transit
 public class Main {
     public static final String BASEGOOGLE = "https://maps.googleapis.com/maps/api/distancematrix/json?language=de&units=metric";
+    public static final String BROWSERURL = "https://www.google.de/maps/dir/Lothstraße+6,+80335+München/";
 
-
-    public static void main(String []args ) throws InterruptedException {
+    public static void main(String []args ) throws InterruptedException, URISyntaxException, IOException {
         List<String > ort = new LinkedList<>();
         List<String> zeit = new LinkedList<>();
         List<String> verkehrsmittel = new LinkedList<>();
@@ -20,6 +23,13 @@ public class Main {
             ort.add(args[i]);
             zeit.add(args[i+1]);
             verkehrsmittel.add(args[i+2]);
+        }
+        if(Desktop.isDesktopSupported())
+        {
+            for (int i=0;i<3;i++) {
+                String uri = BROWSERURL + ort.get(i);
+                Desktop.getDesktop().browse(new URI(uri));
+            }
         }
         while(true){
             for (int i = 0;i<3;i++) {
